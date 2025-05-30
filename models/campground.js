44491cb305824a2931df75483f5985b2ef5076a4
sanyam-kabra <sanyamkabra901@gmail.com>
2025-05-30 10:17:22 +0530

    added basic styles

--- models/campground.js
+++ models/campground.js
@@ -3,8 +3,9 @@ const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
     title: String,
-    price: String,
+    price: Number,
     description: String,
+    image: String,
     location: String
 })
 
--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,14 @@ const seed = async () => {
     for(let i=0;i<50;i++)
     {
         const loc = cities[Math.floor(Math.random()*1000)];
+        const price = Math.floor(Math.random()*20) + 10;
+        const image = `https://picsum.photos/seed/${Math.floor(Math.random() * 1000)}/400`;;
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
             location: `${loc.city}, ${loc.state}`,
+            price: price,
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, maiores! Officia sunt deserunt molestiae consequatur accusamus inventore, veritatis laudantium iusto expedita sint saepe, dolores error perspiciatis totam odio adipisci porro",
+            image: image
         })
         await camp.save();
         console.log(camp);
